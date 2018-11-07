51068fb34350fc264163a097bed459f386a54122
radim <radvecerek@yahoo.co.uk>
2018-11-07 09:00:13 +0100

    dir selection works

--- app/src/main/java/radim/outfit/MainActivity.kt
+++ app/src/main/java/radim/outfit/MainActivity.kt
@@ -1,13 +1,15 @@
 package radim.outfit
 
-import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
 import android.util.Log
-import java.io.File
+import net.rdrei.android.dirchooser.DirectoryChooserActivity
+import net.rdrei.android.dirchooser.DirectoryChooserConfig
 
+
+const val LOG_TAG = "MAIN"
 const val REQUEST_CODE_OPEN_DIRECTORY = 9999
 
 class MainActivity : AppCompatActivity() {
@@ -17,30 +19,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    @Suppress("unused")
     fun directoryPick(@Suppress("UNUSED_PARAMETER")v: View){
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-        intent.addCategory(Intent.CATEGORY_DEFAULT)
-        startActivityForResult(Intent.createChooser(intent, "Choose directory"), REQUEST_CODE_OPEN_DIRECTORY)
+        val chooserIntent = Intent(this, DirectoryChooserActivity::class.java)
+
+        val config = DirectoryChooserConfig.builder()
+                .newDirectoryName("${getString(R.string.app_name)}${getString(R.string.new_directory_name)}")
+                .allowReadOnlyDirectory(false)
+                .allowNewDirectoryNameModification(true)
+                .build()
+
+        chooserIntent.putExtra(DirectoryChooserActivity.EXTRA_CONFIG, config)
+
+        startActivityForResult(chooserIntent, REQUEST_CODE_OPEN_DIRECTORY)
     }
 
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_OPEN_DIRECTORY && resultCode == Activity.RESULT_OK) {
-            try {
-                Log.i("TEST", "Result URI " + data!!.data!!)
-                val pathS = data.data!!.path!!
-                Log.i("TEST", "Result PATH $pathS")
-                val file = File(pathS)
-                Log.i("TEST", "file.absolutePath ${file.absolutePath}")
-                Log.i("TEST", "file.isDirectory ${file.isDirectory}")
-                Log.i("TEST", "file.toString $file")
-            }catch (e: Exception){
-                e.printStackTrace()
-                // TODO
-            }
 
+        if (requestCode == REQUEST_CODE_OPEN_DIRECTORY) {
+            if (resultCode == DirectoryChooserActivity.RESULT_CODE_DIR_SELECTED) {
+                try {
+                    handleDirectoryChoice(data!!
+                            .getStringExtra(DirectoryChooserActivity.RESULT_SELECTED_DIR))
+                }catch(e: Exception){
+                    e.printStackTrace()
+                    //TODO
+                }
+            } else {
+                // Nothing selected
+                //TODO
+            }
         }
     }
 
+    private fun handleDirectoryChoice(selectedDir: String){
+        Log.i(LOG_TAG,"SELECTED_DIR: $selectedDir")
+    }
 }
